907926ca68f69f7d7790ef8e7ec86083ca2a9da7
catzy007 <catzy.rais@gmail.com>
2020-12-11 20:48:48 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Great Bot
+
+### Dependency
+
+#### Software version
+```
+python 3.8.5
+pip 20.0.2
+chrome 87.0.4280.88
+chrome web driver 87.0.4280.88
+```
+
+#### Python library
+```
+pip install pause
+pip install selenium
+pip install beautifulsoup4
+pip install countdown-timer
+```
+
+#### Chrome + web Driver
+<https://chromedriver.chromium.org/downloads>
+<https://www.google.com/chrome/>
+
+### How to use?
+* Make sure `shopee` app installed and logged in on smartphone
+* Install all dependency and software stack
+* Open `bot.py` and change `webdriver.Chrome('webdriver/chromedriver'` to `webdriver.Chrome('webdriver/chromedriver.exe'` if you use MS-Windows
+* Make sure all your things work properly
+* Go to `target.txt` and paste `product link`
+* Open `Bot.py` and adjust `waktumundur()` to flash sale time (reduce 1 to 5 second for load time)
+* Run `Bot.py` and wait
+* Prepare your smartphone and to login with QR code 
+* After successful purchase, login with QR code
+* Then finish your payment
+
+> This code is build and modified from many sources!
---
+++ bot.py
@@ -0,0 +1,46 @@
+import os,random,sys,time
+import pause, datetime
+from selenium.webdriver.common.keys import Keys
+from urllib.parse import urlparse
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+class waktu():
+   def waktumundur():
+        Year    = 2020
+        Month   = 12
+        Day     = 11
+        Hour    = 20
+        Minute  = 38
+        Second  = 00
+        dt = datetime.datetime(Year, Month, Day, Hour, Minute, Second, 000000)
+        print("Start Time ")
+        print(datetime.datetime.now())
+        print("Action Time ")
+        print(dt)
+        pause.until(dt)
+
+class Autocheck():
+    def autocheck(self):
+        print("Bot Running\n")
+        waktu.waktumundur()
+        
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--incognito")
+        global browser
+        browser = webdriver.Chrome('webdriver/chromedriver', options=chrome_options)
+        targetFile = open("target.txt")
+        readFile = targetFile.readlines()
+        targetUrl = readFile[0]
+        browser.get(targetUrl)
+
+        #checkoutid = browser.find_element_by_class_name('btn btn-solid-primary btn--l YtgjXY')
+        #checkoutid = browser.find_element_by_link_text("Beli Sekarang")
+        checkoutid = browser.find_element_by_css_selector('button.YtgjXY')
+        print(checkoutid)
+        checkoutid.click()
+        # checkoutid.send_keys('btn btn-solid-primary btn--l YtgjX')
+        print("\nCheckout!")
+
+run = Autocheck()
+run.autocheck()
