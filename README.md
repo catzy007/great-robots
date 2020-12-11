ba2f19768259b68cfc44a8eb5b24af7db2437760
catzy007 <catzy.rais@gmail.com>
2020-12-12 00:07:34 +0700

    update

--- README.md
+++ README.md
@@ -23,15 +23,14 @@ pip install beautifulsoup4
 <https://www.google.com/chrome/>
 
 ### How to use?
-* Make sure `shopee` app installed and logged in on smartphone
 * Install all dependency and software stack
 * Open `bot.py` and change `webdriver.Chrome('webdriver/chromedriver_87'` to `webdriver.Chrome('webdriver/chromedriver_87.exe'` if you use MS-Windows
 * Make sure all your things work properly
 * Go to `target.txt` and paste `product link`
-* Open `Bot.py` and adjust `waktumundur()` to flash sale time (reduce 1 to 5 second for load time)
+* Open `Bot.py` and adjust `waktumundur1()` to flash sale time (reduce 1 to 5 second for load time)
 * Run `Bot.py` and wait
-* Prepare your smartphone and to login with QR code 
-* After successful purchase, login with QR code
+* After login window appear, proceed to login
+* Wait 120 second and purchase window wil appear
 * Then finish your payment
 
 > This code is build and modified from many sources! dont claim it's only you made it!
--- bot.py
+++ bot.py
@@ -4,16 +4,15 @@ from selenium.webdriver.common.keys import Keys
 from urllib.parse import urlparse
 from selenium import webdriver
 from countdown import countdown
-# from bs4 import BeautifulSoup
 
 class waktu():
-    #Edit time to 60 second (+-5s) before flash-sale
+    #Edit time to -120 second (+-5s) before flash-sale
     def waktumundur1():
         Year    = 2020
         Month   = 12
         Day     = 11
-        Hour    = 22
-        Minute  = 16
+        Hour    = 58
+        Minute  = 00
         Second  = 00
         dt = datetime.datetime(Year, Month, Day, Hour, Minute, Second, 000000)
         print("> START TIME ")
@@ -25,7 +24,7 @@ class waktu():
     def waktumundur2():
         print("\n> LOGIN NOW!")
         menit = 0
-        detik = 5
+        detik = 118
         countdown(menit,detik)
 
 class Autocheck():
@@ -38,40 +37,24 @@ class Autocheck():
         targetUrl = readFile[0]
         
         global browser
-        global browser2
         options = webdriver.ChromeOptions()
         options.add_argument("--incognito")
-        # options.add_argument("--disable-infobars")
         capabilities = options.to_capabilities()
         browser = webdriver.Chrome('webdriver/chromedriver_87', options=options)
-
-        executor_url = browser.command_executor._url
-        session_id = browser.session_id
-        # browser.get("https://shopee.co.id/buyer/login")
-        browser.get("https://www.google.com")
+        browser.get("https://shopee.co.id/buyer/login")
+        # browser.get("https://www.google.com")
 
         waktu.waktumundur2()
 
         browser.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't') 
-
-        browser.get("https://www.youtube.com")
-        
-        # options2 = webdriver.ChromeOptions()
-        # options2.add_argument("--incognito")
-        # options2.add_argument("--disable-infobars")
-        # options2.add_argument("--enable-file-cookies")
-        # options2.add_experimental_option("detach", True)
-        # capabilities2 = options2.to_capabilities()
-        # browser = webdriver.Remote(command_executor=executor_url, desired_capabilities=capabilities2)
-        # # browser.close()
-        # browser.session_id = session_id
-        # # browser2.get(targetUrl)
+        browser.get(targetUrl)
         # browser.get("https://www.youtube.com")
+        # print(browser.find_element_by_class_name('yt-simple-endpoint'))
 
-        # checkoutid = browser.find_element_by_css_selector('button.YtgjXY')
-        # print(checkoutid)
-        # checkoutid.click()
-        # print("\n> Checkout!")
+        checkoutid = browser.find_element_by_css_selector('button.YtgjXY')
+        print(checkoutid)
+        checkoutid.send_keys('btn btn-solid-primary btn--l YtgjX')
+        print("\n> Checkout!")
 
 run = Autocheck()
 run.autocheck()
